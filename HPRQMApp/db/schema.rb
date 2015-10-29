29a7c924b1b437aa0b9fde572fdcd98ce48b2bed
Michael Taylor <90michaeltaylor@gmail.com>
2015-10-28 17:40:23 -0700

    Makes super_admin more secure and adds .swp files to .gitignore.

--- HPRQMApp/db/schema.rb
+++ HPRQMApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028070247) do
+ActiveRecord::Schema.define(version: 20151028071138) do
 
   create_table "admins", force: :cascade do |t|
     t.integer  "project_id"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(version: 20151028070247) do
     t.string   "name",             null: false
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "submitters", force: :cascade do |t|
