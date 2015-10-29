33116d65ec9095b96735b2edc4f48cbd9ab17f69
Ubuntu <ubuntu@ip-172-31-27-32.us-west-2.compute.internal>
2015-10-29 00:37:13 +0000

    Very basic seed file with 1 SuperAdmin and a few regular users

--- HPRQMApp/db/seeds.rb
+++ HPRQMApp/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create(
+  [
+    { f_name: "Smoke", m_name: "Weed", l_name: "Everyday", employee_num: 420, super_admin: true, email: 'SA@SA.com', password:"SA"},
+    { f_name: "User", l_name: "1", employee_num: 1, super_admin: false, email: '1@1.com', password:"1"},
+    { f_name: "User", l_name: "2", employee_num: 2, super_admin: false, email: '2@2.com', password:"2"},
+    { f_name: "User", l_name: "3", employee_num: 3, super_admin: false, email: '3@3.com', password:"3"},
+    { f_name: "User", l_name: "4", employee_num: 4, super_admin: false, email: '4@4.com', password:"4"}
+  ]
+)
