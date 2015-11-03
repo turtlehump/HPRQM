52a406f15ddb7303333dfed9ed350251cd97dcc9
Michael Taylor <90michaeltaylor@gmail.com>
2015-11-02 19:39:45 -0800

    Built many views for reviews and updated seeds.

--- HPRQMApp/db/seeds.cpp
+++ HPRQMApp/db/seeds.cpp
@@ -74,5 +74,38 @@ int main()
   cout << "  ]" << endl;
   cout << ")" << endl;
 
+  cout << "reviews = Review.create(" << endl;
+  cout << "  [" << endl;
+  cout << "    { review_name: \"Upload required artifcats to SDLCM\" }," << endl;
+  cout << "    { review_name: \"Complete RE Project Startup review\" }," << endl;
+  cout << "    { review_name: \"Trigger ALM release from SDLCM\" }," << endl;
+  cout << "    { review_name: \"Complete RE Project Scoping review\" }," << endl;
+  cout << "    { review_name: \"Complete EDW Scoping Architecture Review\" }," << endl;
+  cout << "    { review_name: \"Complete Operational Readiness Review\" }," << endl;
+  cout << "    { review_name: \"Complete RE Analysis review\" }," << endl;
+  cout << "    { review_name: \"Complete Extract Impact Analysis Review\" }," << endl;
+  cout << "    { review_name: \"Complete EDW Architecture Analysis Review\" }," << endl;
+  cout << "    { review_name: \"Complete PII and DER Reviews\" }," << endl;
+  cout << "    { review_name: \"Complete RE Design Review\" }," << endl;
+  cout << "    { review_name: \"Complete DFS Review\" }," << endl;
+  cout << "    { review_name: \"Complete EDW Architecture Design Review\" }," << endl;
+  cout << "    { review_name: \"Complete RE Construct and Test Review\" }," << endl;
+  cout << "    { review_name: \"Obtain DFS sign-off\" }," << endl;
+  cout << "    { review_name: \"Complete all other appropriate IT reviews, as necessary\" }," << endl;
+  cout << "    { review_name: \"Complete Data Model Peer Review\" }," << endl;
+  cout << "    { review_name: \"Complete EDW Architecture Review\" }," << endl;
+  cout << "    { review_name: \"Complete MTI Performance Review\" }," << endl;
+  cout << "    { review_name: \"Complete MTI Review\" }," << endl;
+  cout << "    { review_name: \"Complete RE Acceptance Test Review\" }," << endl;
+  cout << "    { review_name: \"Complete MTP Performance Review\" }," << endl;
+  cout << "    { review_name: \"Complete RFC Review\" }," << endl;
+  cout << "    { review_name: \"Obtain MTP sign-off from business and IT\" }," << endl;
+  cout << "    { review_name: \"Complete RE Implementation Review\" }," << endl;
+  cout << "    { review_name: \"Attach all remaining SDLCM artifacts\" }," << endl;
+  cout << "    { review_name: \"Complete RE Close Out Review\" }" << endl;
+  cout << "  ]" << endl;
+  cout << ")" << endl;
+
 return 0;
 }
+
