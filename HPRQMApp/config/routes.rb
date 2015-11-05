8d2e75df7b7bb71ba30ec543bfe00eb2a22369c6
Michael Taylor <90michaeltaylor@gmail.com>
2015-11-05 09:33:29 -0800

    Fixed a few odds and ends with routes and dashboard.

--- HPRQMApp/config/routes.rb
+++ HPRQMApp/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :realeases, shallow: true
   end
   resources :reviews
+  resources :questions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
