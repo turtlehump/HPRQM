cadd1da5e74d31bb0ce597030106718d0a7dc646
Philip Vierhus <pvierhus@gmail.com>
2015-11-09 22:14:08 +0000

    Added a userID column to the answers so we can keep track of who answers what questions

--- HPRQMApp/db/migrate/20151106075358_create_answers.rb
+++ HPRQMApp/db/migrate/20151106075358_create_answers.rb
@@ -7,6 +7,7 @@ class CreateAnswers < ActiveRecord::Migration
       t.boolean :bool
       t.references :question, index: true, foreign_key: true
       t.references :release, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
