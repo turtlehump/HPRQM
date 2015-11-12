1e6efc622968a0fed7daa2e77ff3c43912eb811e
Michael Taylor <90michaeltaylor@gmail.com>
2015-11-12 00:08:01 -0800

    Admin can almost edit other users.

--- HPRQMApp/app/controllers/users_controller.rb
+++ HPRQMApp/app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: [:index, :show, :new, :edit]
+  before_action :set_user, only: [:show, :new, :edit]
+
   def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+  def get_user
+    @current_user = current_user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
