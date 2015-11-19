78e4b874bbada511e6a6ce1f4c1e9e78d43899e4
Michael Taylor <90michaeltaylor@gmail.com>
2015-11-19 09:24:47 -0800

    Fixed the routes the way I meant to change them to.

--- HPRQMApp/config/routes.rb
+++ HPRQMApp/config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     end
   end
   
-  get 'releases', to: 'releases#show'
+  get 'releases', to: 'releases#index'
   resources :questions
 
   # The priority is based upon order of creation: first created -> highest priority.
